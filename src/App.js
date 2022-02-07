93d2819b49958d885446764849d3fc3f22fd13c3
Weasel <cody.a.tracey@gmail.com>
2022-02-06 18:42:24 -0600

    Added in basic scaffolding for react app and SUUUUUPER basic landing page

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Container, Row, Col } from 'react-bootstrap'
+const App = () => {
+  return (
+    <div className='mt-5'>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Thank you for your interest in my page, it is currently under construction and will be up soon!</h1>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+
+  );
+}
+
+export default App;
