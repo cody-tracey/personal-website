87f9bdfb2929dbcd748669fd53f9f63b14d9e848
Weasel <cody.a.tracey@gmail.com>
2022-03-09 23:13:04 -0600

    added in the profile image, and a new assets folder, starting to re-write my bio

--- src/Components/Bio/bio.js
+++ src/Components/Bio/bio.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
+import profile from '../../assets/profile.jpg'
 
 const Bio = () => {
     return (
-        <div className="drop">
+        <div className="drop-small">
             <Container className="main">
                 <Row className='text-center'>
                     <u><h1>About Me</h1></u>
                 </Row>
                 <Row>
                     <Col>
-                        <Image src="#" rounded alt='Cody Tracey - Developer Extraordinaire' />
-                    </Col>
-                    <Col>
-                        My name is Cody Tracey, and thank you for taking the time to view my
+                        <img src={profile}  alt='Cody Tracey - Developer Extraordinaire' className='profile-pic'/>
+                        <p>My name is Cody Tracey, and thank you for taking the time to view my
                         portfolio. I am originally from the Chicagoland area but moved to
                         Nasvhille 4 years ago after graduating college. I attended Carthage
                         College in Kenosha, WI where I double majored in Classical Studies and
@@ -24,7 +23,9 @@ const Bio = () => {
                         After graduating I worked as a project manager at Card Marketing Services (CMS), while attendingthe Vanderbilt/Trilogy Education Full Stack Web
                         Developing Bootcamp, upon completetion I obtained a job working as a coding instructor as part of the <a href="perseverenow.org" tagret="_blank">Persevere Program</a>.
                         <br />
-                        I am proficient in: HTML5, CSS3, JavaScript, jQuery, Bootstrap, NodeJS, MySQL, MongoDB, MySQL, Express, ReactJS.</Col>
+                        I am proficient in: HTML5, CSS3, JavaScript, jQuery, Bootstrap, NodeJS, MySQL, MongoDB, MySQL, Express, ReactJS.
+                        </p>
+                        </Col>
                 </Row>
             </Container>
         </div>
