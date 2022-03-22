e07be9d53025891943a19734dd26ba7e6c081c76
Weasel <cody.a.tracey@gmail.com>
2022-03-21 21:09:32 -0500

    added in bootstrap form, styled all of it but the submit button

--- src/Components/Contact/contact.js
+++ src/Components/Contact/contact.js
@@ -1,15 +1,77 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Form, Row, Col, Button } from 'react-bootstrap';
 
 const Contact = () => {
+
+    const [errors, setErrors] = useState({
+        "name": "",
+        "email": "",
+        "message": ""
+    });
+    // const [status, setStatus] = useState("Submit");
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     setStatus("Sending...");
+    //     const { name, email, message } = e.target.elements;
+    //     let details = {
+    //         name: name.value,
+    //         email: email.value,
+    //         message: message.value,
+    //     };
+    // };
+    // let response = await fetch("http://localhost:5000/contact", {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json;charset=utf-8",
+    //     },
+    //     body: JSON.stringify(details),
+    // });
+    // setStatus("Submit");
+    // let result = await response.json();
+
+
+
     return (
         <div className="drop">
             <Container className="main" >
-                <Row>
-                    <Col>
-                        <h1>Contact page will go here</h1>
+                <Row className='pt-2 pb-2'>
+                    <Col className='text-center'>
+                        <h1>CONTACT ME</h1>
                     </Col>
                 </Row>
+                <Form>
+                    <Row className='pt-2 pb-2'>
+                        <Col md={6} sm={12}>
+                            <Form.Group>
+                                <Form.Label>Name:</Form.Label>
+                                <Form.Control type='text' placeholder='Name' />
+                                {errors.name ? <p>{errors.name}</p> : ""}
+                            </Form.Group>
+                        </Col>
+                        <Col md={6} sm={12}>
+                            <Form.Group>
+                                <Form.Label>Email:</Form.Label>
+                                <Form.Control type='email' placeholder='Email' />
+                                {errors.email ? <p>{errors.email}</p> : ""}
+
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row className='pt-2 pb-2'>
+                        <Col>
+                            <Form.Group>
+                                <Form.Label>Message:</Form.Label>
+                                <Form.Control as='textarea' rows={3} />
+                                {errors.message ? <p>{errors.message}</p> : ""}
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row className='pt-2 pb-2'>
+                        <Col xs={1}>
+                            <Button>Submit</Button>
+                        </Col>
+                    </Row>
+                </Form>
             </Container>
         </div>
 
