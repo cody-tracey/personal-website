9b4a1f873b7cc0f31c1766669047159666b1af7c
Weasel <cody.a.tracey@gmail.com>
2022-03-24 21:59:14 -0500

    added in first draft bio, icons to socials, and pseudo class for links

--- src/Components/Bio/bio.js
+++ src/Components/Bio/bio.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import profile from '../../assets/profile.jpg'
-
 import Navi from '../Navi/navi';
+
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 
 const Bio = () => {
     return (
@@ -18,11 +19,17 @@ const Bio = () => {
                     </Col>
                     <Col s={12} md={7} l={8} xl={8}>
                         <p className='bio'>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec adipiscing tristique risus nec feugiat in fermentum posuere urna. Fermentum et sollicitudin ac orci phasellus egestas. Mauris ultrices eros in cursus turpis massa tincidunt dui ut. Auctor eu augue ut lectus. Nec nam aliquam sem et tortor consequat id porta. Blandit aliquam etiam erat velit scelerisque in. Pretium quam vulputate dignissim suspendisse in. Ullamcorper sit amet risus nullam eget felis eget nunc lobortis. Venenatis lectus magna fringilla urna. Turpis tincidunt id aliquet risus feugiat in ante. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mauris cursus mattis molestie a. Amet dictum sit amet justo. Commodo nulla facilisi nullam vehicula ipsum a.
+                            Hey, there my name is Cody (he/him) and thank you for checking me out! Originally from the Chicagoland area but moved to Nashville 10 years ago. I attended Carthage College where I double majored in Classical Studies and Religious Studies.
                             <br />
-                            Aliquet porttitor lacus luctus accumsan tortor posuere. At consectetur lorem donec massa sapien faucibus. Fringilla ut morbi tincidunt augue interdum. Et molestie ac feugiat sed lectus vestibulum mattis. Duis at consectetur lorem donec. Felis imperdiet proin fermentum leo vel orci. Lacus luctus accumsan tortor posuere ac. Diam vel quam elementum pulvinar etiam non. Curabitur gravida arcu ac tortor dignissim convallis. Quam vulputate dignissim suspendisse in est ante in.
+                            Upon graduation I worked in as an Account Specialist at Card Market while attending Vanderbilt's Coding Bootcamp. After which I taught Web Development for a year as part of the <a href='https://perseverenow.org/' target='_blank' rel="noreferrer" ><b>PERSEVERE</b></a> program. Currently now I work as an IT Project Manager at Card Market, while moonlighting as a budding web developer (MERN).
                             <br />
-                            Nunc sed blandit libero volutpat. Nec ullamcorper sit amet risus nullam. Ut consequat semper viverra nam. Felis eget velit aliquet sagittis id consectetur. Elit duis tristique sollicitudin nibh sit amet commodo nulla. Fringilla urna porttitor rhoncus dolor purus non enim. Sapien pellentesque habitant morbi tristique senectus et netus. Erat velit scelerisque in dictum non consectetur. At lectus urna duis convallis convallis tellus. Ligula ullamcorper malesuada proin libero nunc consequat interdum. Laoreet suspendisse interdum consectetur libero id. Id diam maecenas ultricies mi eget mauris.
+                            In my spare time I enjoy spoiling my dogs, lifting weights, crushing my enemies at Magic the Gathering, and playing Dungeons and Dragons.
+                        </p>
+                        <p>
+                            Check me out on social media:
+                            <br />
+                            <a href='https://github.com/cody-tracey' target='_blank' rel='noreferrer' className='icon' ><FaGithubSquare size={70} /></a>
+                            <a href='https://www.linkedin.com/in/cody-tracey/' target='_blank' rel='noreferrer' className='icon' ><FaLinkedin size={70} /></a>
                         </p>
                     </Col>
                 </Row>
--- src/Components/Navi/navi.js
+++ src/Components/Navi/navi.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Nav, Navbar, Container } from "react-bootstrap";
+import { Nav, Navbar} from "react-bootstrap";
 
 const Navi = () => {
     return (
